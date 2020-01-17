361ca6aa2093f354366dfd03143de5511b9cde14
dary16 <594217916@qq.com>
2020-01-17 09:42:51 +0800

    错误提示
    错误提示

--- src/http/index.js
+++ src/http/index.js
@@ -1,9 +1,7 @@
 // 封装请求
 import api from './api';
 import axios from 'axios';
-import {
-    Toast
-} from 'vant';
+import { Toast } from 'vant';
 import store from '../store';
 
 // 全局设置
@@ -16,82 +14,101 @@ axios.interceptors.request.use(config => {
     return config;
 });
 
-// 拦截响应response，并做一些错误处理
-axios.interceptors.response.use(
-    response => {
-        console.log('response');
-        const data = response.data;
-
-        return data;
-    },
-    err => {
-        // 这里是返回状态码不为200时候的错误处理
-        if (err && err.response) {
-            switch (err.response.status) {
-                case 400:
-                    err.message = '请求错误';
-                    break;
-
-                case 401:
-                    err.message = '未授权，请登录';
-                    break;
-
-                case 403:
-                    err.message = '拒绝访问';
-                    break;
-
-                case 404:
-                    err.message = `请求地址出错: ${err.response.config.url}`;
-                    break;
-
-                case 408:
-                    err.message = '请求超时';
-                    break;
-
-                case 500:
-                    err.message = '服务器内部错误';
-                    break;
-
-                case 501:
-                    err.message = '服务未实现';
-                    break;
-
-                case 502:
-                    err.message = '网关错误';
-                    break;
-
-                case 503:
-                    err.message = '服务不可用';
-                    break;
-
-                case 504:
-                    err.message = '网关超时';
-                    break;
-
-                case 505:
-                    err.message = 'HTTP版本不受支持';
-                    break;
-
-                default:
-                    err.message = '未知错误';
-            }
-        }
-        return Promise.reject(err);
-    }
-);
-
 const htp = axios.create({
     baseURL: '/',
     timeout: 10000
 });
 
+/**
+ * 提示函数
+ * 禁止点击蒙层、显示一秒后关闭
+ */
+const tip = msg => {
+    Toast({
+        message: msg,
+        duration: 1000,
+        forbidClick: true
+    });
+};
+
+/**
+ * 请求失败后的错误统一处理
+ * @param {Number} status 请求失败的状态码
+ */
+const errorHandle = status => {
+    //状态码判断
+    switch (status) {
+        case 400:
+            tip('请求错误');
+            break;
+
+        case 401:
+            tip('未授权，请登录');
+            break;
+
+        case 403:
+            tip('拒绝访问');
+            break;
+
+        case 404:
+            tip(`请求地址出错: ${err.response.config.url}`);
+            break;
+
+        case 408:
+            tip('请求超时');
+            break;
+
+        case 500:
+            tip('服务器内部错误');
+            break;
+
+        case 501:
+            tip('服务未实现');
+            break;
+
+        case 502:
+            tip('网关错误');
+            break;
+
+        case 503:
+            tip('服务不可用');
+            break;
+
+        case 504:
+            tip('网关超时');
+            break;
+
+        case 505:
+            tip('HTTP版本不受支持');
+            break;
+
+        default:
+            tip('未知错误');
+    }
+};
+
+// 拦截响应response，并做一些错误处理
+htp.interceptors.response.use(
+    res => {
+        res.status === 200 ? Promise.resolve(res) : Promise.reject(res);
+        return res;
+    },
+    err => {
+        const { response } = err;
+        // 这里是返回状态码不为200时候的错误处理
+        if (response) {
+            //请求已发出，但是不在2XX的范围
+            errorHandle(response.status);
+            return Promise.reject(response);
+        } else {
+            return Promise.reject(response);
+        }
+    }
+);
+
 // 处理接口规范的公有方法
 // 处理接口规范的公有方法
-const xhr = ({
-    method = 'post',
-    ur,
-    options
-}) => {
+const xhr = ({ method = 'post', ur, options }) => {
     let p,
         m = false;
     let load = {
@@ -109,7 +126,6 @@ const xhr = ({
     let ops = '';
     let unOps = {};
 
-
     if (store.state.userInfo) {
         unOps = JSON.parse(JSON.stringify(store.state.userInfo));
         if (method == 'get') {
@@ -123,30 +139,8 @@ const xhr = ({
     }
     switch (method) {
         case 'get':
-            p = new Promise(function (resolve, reject) {
-                htp
-                    .get(api[ur], ops)
-                    .then(
-                        response => {
-                            m = true;
-                            load.close();
-                            if (response.data && response.status == 200) {
-                                resolve(response.data);
-                            } else {
-                                reject(response.data.msg);
-                            }
-                        },
-                        er => {
-                            m = true;
-                            load.close();
-                            errHandler(er);
-                        }
-                    );
-            });
-            break;
-        case 'post':
-            p = new Promise(function (resolve, reject) {
-                htp.post(api[ur], ops).then(
+            p = new Promise(function(resolve, reject) {
+                htp.get(api[ur], ops).then(
                     response => {
                         m = true;
                         load.close();
@@ -159,7 +153,27 @@ const xhr = ({
                     er => {
                         m = true;
                         load.close();
-                        errHandler(er);
+                        errHandler(er.status);
+                    }
+                );
+            });
+            break;
+        case 'post':
+            p = new Promise(function(resolve, reject) {
+                htp.post(api[ur], ops).then(
+                    response => {
+                        m = true;
+                        load.close();
+                        if (response.data && response.status == 200) {
+                            resolve(response.data);
+                        } else {
+                            reject(response.status);
+                        }
+                    },
+                    er => {
+                        m = true;
+                        load.close();
+                        errorHandle(er.status);
                     }
                 );
             });
@@ -169,11 +183,5 @@ const xhr = ({
     }
     return p;
 };
-
-function errHandler(er) {
-    Toast({
-        message: '网络连接错误,请检查您的网络连接是否正常'
-    });
-}
 
 export default xhr;
